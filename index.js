03fe4f75c6609eb75e7331ae51b0a7c0659b19ca
Stuart Williams <stuart.williams@bbc.co.uk>
2017-04-21 22:09:42 +0100

    Updated index with nfcall and nfapply

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const allSettled = require('./all-settiled')
+const nfcall = require('./nfcall')
+const nfapply = require('./nfapply')
 
 module.exports = {
-  allSettled
+  allSettled,
+  nfcall,
+  nfapply
 }
