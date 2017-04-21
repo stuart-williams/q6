686b19d63d5c13aece393ee15d0759fc1eee3d7f
Stuart Williams <stuart.williams@bbc.co.uk>
2017-04-21 22:07:05 +0100

    Implemented nfapply

---
+++ nfapply.js
@@ -0,0 +1,10 @@
+function nfapply (fn, args) {
+  return new Promise((resolve, reject) => {
+    fn(...[ ...args, (error, response) => {
+      if (error) return reject(error)
+      resolve(response)
+    } ])
+  })
+}
+
+module.exports = nfapply
---
+++ test/nfapply-test.js
@@ -0,0 +1,20 @@
+const { assert } = require('chai')
+const { readFile } = require('fs')
+const { resolve } = require('path')
+const nfapply = require('../nfapply')
+
+describe('nfapply', () => {
+  it('should resolve as expected when params are valid', (done) => {
+    nfapply(readFile, [ resolve(__dirname, './data/hello.txt'), 'utf8' ]).then((contents) => {
+      assert.isString(contents)
+      done()
+    })
+  })
+
+  it('should reject as expected when params are invalid', (done) => {
+    nfapply(readFile, [ resolve(__dirname, 'nope.txt'), 'utf8' ]).catch((error) => {
+      assert.equal(true, true)
+      done()
+    })
+  })
+})
