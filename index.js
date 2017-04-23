d599e39c247d210a4401dbfc69a6fd90a83892ff
Stuart Williams <stuart.williams@bbc.co.uk>
2017-04-23 10:28:31 +0100

    Added any to index

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const nfcall = require('./nfcall')
 const nfapply = require('./nfapply')
 const fcall = require('./fcall')
 const fapply = require('./fapply')
+const any = require('./any')
 
 module.exports = {
   allSettled,
   nfcall,
   nfapply,
   fcall,
-  fapply
+  fapply,
+  any
 }
