d4717fa7bb7ac48333b1e7317527aa73a4724f5c
Stuart Williams <stuart.williams@bbc.co.uk>
2017-05-30 21:48:47 +0100

    Improved readability of allSettled

--- all-settled.js
+++ all-settled.js
@@ -1,6 +1,14 @@
-const wrap = (promise) => Promise.resolve(promise)
-  .then((value) => ({ state: 'fulfilled', value }))
-  .catch((reason) => ({ state: 'rejected', reason }))
+const wrap = (promise) => {
+  return Promise.resolve(promise)
+    .then((value) => ({
+      state: 'fulfilled',
+      value
+    }))
+    .catch((reason) => ({
+      state: 'rejected',
+      reason
+    }))
+}
 
 function allSettled (promises) {
   return Promise.all(promises.map(wrap))
